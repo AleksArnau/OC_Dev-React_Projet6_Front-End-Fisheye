e645002fe3d21b7736f87b6da42afa6a741de64c
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2023-09-06 14:57:43 +0200

    created new template and minor fixes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,38 @@
 //Mettre le code JavaScript lié à la page photographer.html
+let url = new URL(location.href);
+let kanapPageId = url.searchParams.get("id");
+
+//fetches photographers' data and picture/video properties
+async function getPhotographers() {
+  return fetch("../data/photographers.json")
+    .then((res) => res.json())
+    .catch(console.error);
+}
+
+//fetches a photographer's data etc.
+async function getPhotographer() {
+  return fetch("../data/photographers.json")
+    .then((res) => res.json())
+    .then((object) => object.id) //TODO
+    .catch(console.error);
+}
+
+//displays all the photographers' info in the DOM from a set
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".select_photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+//runs the fetch and display functions
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
---
+++ scripts/templates/photographers.js
@@ -0,0 +1,48 @@
+function photographerTemplate(data) {
+  const { name, portrait, tagline, price, city, country, id } = data;
+
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    const articlePhotographer = document.createElement("article");
+    articlePhotographer.setAttribute("class", "photographerArticle");
+
+    const linkPhotographer = document.createElement("a");
+    linkPhotographer.setAttribute("href", `./photographer.html?id=${id}`);
+    linkPhotographer.setAttribute("class", "photographerLink");
+
+    const imgPicture = document.createElement("img");
+    imgPicture.setAttribute("class", "photographerImg");
+    imgPicture.setAttribute("src", picture);
+
+    const h2Name = document.createElement("h2");
+    h2Name.setAttribute("class", "photographerH2");
+    h2Name.textContent = name;
+
+    const divDetails = document.createElement("div");
+    divDetails.setAttribute("class", "photographerDetails");
+
+    const pLocation = document.createElement("p");
+    pLocation.setAttribute("class", "photographerLocation");
+    pLocation.textContent = city + ", " + country;
+
+    const pTagline = document.createElement("p");
+    pTagline.setAttribute("class", "photographerTagline");
+    pTagline.textContent = tagline;
+
+    const pPrice = document.createElement("p");
+    pPrice.setAttribute("class", "photographerPrice");
+    pPrice.textContent = price + "€/jour";
+
+    articlePhotographer.appendChild(linkPhotographer);
+    articlePhotographer.appendChild(divDetails);
+    linkPhotographer.appendChild(imgPicture);
+    linkPhotographer.appendChild(h2Name);
+    divDetails.appendChild(pLocation);
+    divDetails.appendChild(pTagline);
+    divDetails.appendChild(pPrice);
+
+    return articlePhotographer;
+  }
+  return { name, picture, getUserCardDOM };
+}
