4bed591fbd789c775feb45790dc92e4bfdab1ed5
Aleksandar Arnautovic <Aleksandar96@hotmail.fr>
2023-09-14 07:09:54 +0200

    photoDOM display functionnal

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -50,48 +50,38 @@ function photographerTemplate(data) {
 function photoTemplate(data) {
   const { id, photographerId, title, image, likes, date, price } = data;
 
-  const picture = `assets/photographers/${image}`;
+  const picture = `assets/images/${image}`;
 
   function getPhotoCardDOM() {
-    const articlePhotographer = document.createElement("article");
-    articlePhotographer.setAttribute("class", "photographerArticle");
+    const articlePhoto = document.createElement("article");
+    articlePhoto.setAttribute("class", "articlePhoto");
 
-    const linkPhotographer = document.createElement("a");
-    linkPhotographer.setAttribute("href", `./photographer.html?id=${id}`);
-    linkPhotographer.setAttribute("class", "photographerLink");
+    const linkPhoto = document.createElement("a");
+    linkPhoto.setAttribute("href", `./photo.html?id=${id}`);
+    linkPhoto.setAttribute("class", "photoLink");
 
     const imgPicture = document.createElement("img");
-    imgPicture.setAttribute("class", "photographerImg");
+    imgPicture.setAttribute("class", "photoImg");
     imgPicture.setAttribute("src", picture);
 
-    const h2Name = document.createElement("h2");
-    h2Name.setAttribute("class", "photographerH2");
-    h2Name.textContent = name;
-
     const divDetails = document.createElement("div");
-    divDetails.setAttribute("class", "photographerDetails");
+    divDetails.setAttribute("class", "photoDetails");
 
-    const pLocation = document.createElement("p");
-    pLocation.setAttribute("class", "photographerLocation");
-    pLocation.textContent = city + ", " + country;
+    const h3Title = document.createElement("h3");
+    h3Title.setAttribute("class", "photoH3");
+    h3Title.textContent = title;
 
-    const pTagline = document.createElement("p");
-    pTagline.setAttribute("class", "photographerTagline");
-    pTagline.textContent = tagline;
+    const pLikes = document.createElement("p");
+    pLikes.setAttribute("class", "photoLikes");
+    pLikes.textContent = likes + " likes";
 
-    const pPrice = document.createElement("p");
-    pPrice.setAttribute("class", "photographerPrice");
-    pPrice.textContent = price + "€/jour";
+    articlePhoto.appendChild(linkPhoto);
+    articlePhoto.appendChild(divDetails);
+    linkPhoto.appendChild(imgPicture);
+    divDetails.appendChild(h3Title);
+    divDetails.appendChild(pLikes);
 
-    articlePhotographer.appendChild(linkPhotographer);
-    articlePhotographer.appendChild(divDetails);
-    linkPhotographer.appendChild(imgPicture);
-    linkPhotographer.appendChild(h2Name);
-    divDetails.appendChild(pLocation);
-    divDetails.appendChild(pTagline);
-    divDetails.appendChild(pPrice);
-
-    return imgPhoto;
+    return articlePhoto;
   }
   return { title, picture, getPhotoCardDOM };
 }
